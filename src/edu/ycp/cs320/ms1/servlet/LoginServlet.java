5a02bbd16a710bbe6221fab99657fd6ca907997b
Matt Ritchie <mritchie1@ycp.edu>
2017-03-20 00:28:09 -0400

    Login successfully loops with error message if wrong credentials are entered, redirects properly to "UserHome" page (not included) upon good creds.

--- src/edu/ycp/cs320/ms1/servlet/LoginServlet.java
+++ src/edu/ycp/cs320/ms1/servlet/LoginServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import edu.ycp.cs320.ms1.controller.NumbersController;
 import edu.ycp.cs320.ms1.model.Numbers;
-//I can mess with your shit now, hahahaha
+
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -22,8 +22,24 @@ public class LoginServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		Numbers model = new Numbers();
+		//Numbers model = new Numbers();
 		
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		String errorMessage = null;
+		
+		if(username.equals("Test") && password.equals("test1")){
+			req.getSession().setAttribute("username", username);
+			resp.sendRedirect(req.getContextPath() + "/UserHome");
+		}
+		else{
+			errorMessage = "Invalid Login.";
+			req.setAttribute("errorMessage", errorMessage);
+			req.getRequestDispatcher("/_view/login.jsp").forward(req, resp);
+		}
+		
+		
+		/*
 		// Decode form parameters and dispatch to controller
 		String errorMessage = null;
 		//Double result = null;
@@ -38,7 +54,7 @@ public class LoginServlet extends HttpServlet {
 			//Double second = model.getValue2();
 
 			if (first == null || second == null) {
-				errorMessage = "Please specify two numbers";
+				errorMessage = "Invalid";
 			} else {
 				NumbersController controller = new NumbersController();
 				model.setResult(controller.mult(first, second));
@@ -58,7 +74,11 @@ public class LoginServlet extends HttpServlet {
 		
 		// Forward to view to render the result HTML document
 		req.getRequestDispatcher("/_view/login.jsp").forward(req, resp);
+		
+	*/
 	}
+
+		
 	private Double getDouble(HttpServletRequest req, String name) {
 		return Double.parseDouble(req.getParameter(name));
 		
