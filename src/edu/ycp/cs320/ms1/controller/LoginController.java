97205e806207a1c685265c986d87f0ece0f031a0
Liam <lhurd@ycp.edu>
2017-04-12 14:09:54 -0400

    Started refining logic controller.

--- src/edu/ycp/cs320/ms1/controller/LoginController.java
+++ src/edu/ycp/cs320/ms1/controller/LoginController.java
@@ -1,5 +1,20 @@
 package edu.ycp.cs320.ms1.controller;
 
+import edu.ycp.cs320.ms1.model.User;
+
 public class LoginController {
 	
+	//private User model = null;
+	
+	/*public LogicController(User model){
+		this.model = model;
+	}
+	
+	public boolean checkUserName(String name){
+		return model.validateUserName(name);
+	}
+	
+	public boolean validateCredentials(String name, String pw){
+		return model.validatePW(name, pw);
+	}*/
 }
