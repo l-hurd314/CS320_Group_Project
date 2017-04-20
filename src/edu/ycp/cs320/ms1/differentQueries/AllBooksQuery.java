c016caaedb1d143c8f06babd9ae8a822bfc23215
Matt Ritchie <mritchie1@ycp.edu>
2017-04-20 01:58:24 -0400

    Made AllBooksQuery stop showing red marks.

--- src/edu/ycp/cs320/ms1/differentQueries/AllBooksQuery.java
+++ src/edu/ycp/cs320/ms1/differentQueries/AllBooksQuery.java
@@ -11,7 +11,7 @@ import edu.ycp.cs320.ms1.persist.DatabaseProvider;
 import edu.ycp.cs320.ms1.persist.IDatabase;
 
 public class AllBooksQuery {
-	public static void main(String[] args) throws Exception {
+	/*public static void main(String[] args) throws Exception {
 		Scanner keyboard = new Scanner(System.in);
 
 		// Create the default IDatabase instance
@@ -19,18 +19,18 @@ public class AllBooksQuery {
 		
 		// get the DB instance and execute transaction
 		IDatabase db = DatabaseProvider.getInstance();
-		List<Pair<Author, Book>> authorBookList = db.findAllBooksWithAuthors();
+		List<Pair<User, Book>> authorBookList = db.findAllBooksWithAuthors();
 		
 		// check if anything was returned and output the list
 		if (authorBookList.isEmpty()) {
 			System.out.println("There are no books in the database");
 		}
 		else {
-			for (Pair<Author, Book> authorBook : authorBookList) {
-				Author author = authorBook.getLeft();
-				Book book = authorBook.getRight();
+			for (Pair<User, Book> authorBook : authorBookList) {
+				User user = authorBook.getLeft();
+				Book post = authorBook.getRight();
 				System.out.println(book.getTitle() + ", " + book.getIsbn() + ", " + author.getLastname() + ", " + author.getFirstname());
 			}
 		}
-	}
+	}*/
 }
